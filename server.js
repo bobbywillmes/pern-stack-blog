38030524818a70fb979448e8e68c37e36464ace3
bobbywillmes <bobbywillmes@gmail.com>
2024-02-27 07:38:49 -0800

    connect react to backend

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
-    res.send('Hello from the backend :)');
+app.get('/message', (req, res) =>{
+    res.send('Hello from the backend :) Back here Node.js is running the entire show.');
 })
 
 app.listen(port, () => {
